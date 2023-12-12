ee752524efbdc05a5bab7d479340ca137f4e9b55
Baxolise Ndzobngo <baxolisendzobongo222@gmail.com>
2023-12-12 14:08:21 +0200

    changed send mail script

--- script.js
+++ script.js
@@ -57,3 +57,22 @@ $(document).ready(function () {
         }
     });
 });
+
+    function sendmail(event) {
+        event.preventDefault();
+    let params = {
+        fullname: document.getElementById("fullName").value,
+    email: document.getElementById("email_id").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+            }
+
+    emailjs.send("service_ms7thw7", "template_e2q04bg", params)
+    .then(function (res) {
+        alert("Success! " + res.status);
+                })
+    .catch(function (error) {
+        alert("Error sending message. Please try again later.");
+    console.error("Error sending email:", error);
+                });
+        }
